3ac6307b78bacc285e57210f472a85865380743b
Garcia <hisa.garcia@nokia.com>
2020-11-11 21:33:55 -0600

    Signed-off-by: Garcia <hisa.garcia@nokia.com>

---
+++ src/test/java/com/qa/testcases/WikiPage.java
@@ -0,0 +1,27 @@
+package com.qa.testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WikiPage {
+	
+	@Test // it is used to execute our testcases
+	public void createAcount() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\qpsq01\\eclipse-workspace\\DevOps\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://github.com/edureka-devops/selenium-website");
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+		//write the test steps 
+		driver.findElement(By.linkText("Sign in")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.name("login")).sendKeys("hisa501@gmail.com");
+	
+	}
+}
